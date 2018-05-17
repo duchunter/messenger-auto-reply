4f8a6f219eb2902a6db89e8eab043e62746a7f98
Gangplank <duc71098@gmail.com>
2018-05-17 09:32:48 +0700

    Chatbot now available

--- server/controller/setTimer.js
+++ server/controller/setTimer.js
@@ -2,7 +2,7 @@
 
 import request from "request";
 import login from "facebook-chat-api";
-import handleInbox from '../utils/handleInbox';
+import apiai from '../utils/apiai';
 import { addLog } from '../utils/log';
 import { scanTable, updateInTable } from '../models/models';
 
@@ -131,9 +131,76 @@ export default async function (req, res) {
 
     // Listen to incoming message
     api.listen((err, message) => {
-      console.log(1);
-      console.log(message);
-      handleInbox(api, err, message, answeredThreads, chatThreads);
+      if (!answeredThreads[message.threadID]) {
+        // Answer group chat only when being mentioned, if not, return
+        if (message.isGroup) {
+          if (message.mentions) {
+            if (!message.mentions[api.getCurrentUserID()]) { return; }
+          }
+        }
+
+        // Mark as aswered
+        answeredThreads[message.threadID] = true;
+
+        // Get user data to fetch msg
+        scanTable({
+          table: 'Accounts',
+          condition: { email }
+        }).then(users => {
+          // User not exist (somehow) -> reply with default msg
+          if (users.length == 0) {
+            addLog({
+              code: 'error',
+              content: 'Reply: cannot find user info of ' + email
+            });
+            api.sendMessage(defaultMsg, message.threadID, (err, info) => {
+              api.sendMessage(
+                `BOT: If you want chat with me, type 'i want to chat'`,
+                message.threadID
+              );
+            });
+
+          // Valid user -> reply with msg if still active
+          } else {
+            if (users[0].stop > new Date().getTime()) {
+              // Reply with msg
+              let msg = users[0].msg || defaultMsg;
+              msg = (msg == 'default') ? defaultMsg : 'BOT: ' + msg;
+              api.sendMessage(msg, message.threadID, (err, info) => {
+                api.sendMessage(
+                  `BOT: If you want chat with me, type 'i want to chat'`,
+                  message.threadID
+                );
+              });
+            }
+          }
+        });
+      }
+
+      // If sender is chatting with bot
+      if (chatThreads[message.threadID]) {
+        apiai(message.body).then(text => {
+          api.sendMessage(`BOT: ${text}`, message.threadID);
+        });
+      }
+
+      // If sender want to chat
+      if (!message.isGroup && message.body.toLowerCase() == 'i want to chat') {
+        chatThreads[message.threadID] = true;
+        api.sendMessage(
+          `BOT: Great! Now i will reply to your message :), type 'bye' to stop`,
+          message.threadID
+        );
+      }
+
+      // If sender want to stop chatting
+      if (!message.isGroup && message.body.toLowerCase() == 'bye') {
+        chatThreads[message.threadID] = false;
+        api.sendMessage(
+          `BOT: You can type 'i want to chat' anytime to chat with me again :)`,
+          message.threadID
+        );
+      }
     });
   });
 }
